08c18d446144c5aee76c2733a4cdb76d461704d3
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2017-09-21 11:42:19 +0100

    seperate api into seperate file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const bodyParser  = require('body-parser');
-const RedisStorage = require('./RedisStorage/RedisService');
+const api = require('./api');
 
 const app = express();
 
@@ -13,47 +12,7 @@ app.use(bodyParser.json());
 app.set('secret', process.env.JWT_SECRET)
 
 const port = process.env.PORT;
-const router = express.Router();
 
-app.use('/api',router);
-
-router.use((req, res, next) => {
-
-  function getFailureMessage() {
-    return {
-      success: false,
-      message: 'No token provided.'
-    };
-  }
-
-  // check header or url parameters or post parameters for token
-  if(req.headers.authorization === undefined || req.headers.authorization.split(' ').length !== 2){
-    return res.status(403).send(getFailureMessage());
-  }
-  var token = req.headers.authorization.split(' ')[1];
-
-  if (token) {
-
-    jwt.verify(token, app.get('secret'), function(err, decoded) {
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-
-  } else {
-    return res.status(403).send(getFailureMessage());
-  }
-});
-
-router.get('/clients',function(req, res) {
-  var redisStorage = new RedisStorage();
-
-  redisStorage.GetClients().then((clients) => {
-    res.send(clients);
-  });
-});
+app.use('/api',api(app.get('secret')));
 
 app.listen(port);

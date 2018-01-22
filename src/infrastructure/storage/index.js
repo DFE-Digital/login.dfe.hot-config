d195ea8264f1e2830f77cd1529a26da9626503cc
Simon Bull <simon.bull.84@googlemail.com>
2018-01-22 15:17:07 +0000

    Add azure blob adapter

--- src/infrastructure/storage/index.js
+++ src/infrastructure/storage/index.js
@@ -3,6 +3,8 @@ const config = require('./../config');
 let adapter;
 if (config.storage.type === 'redis') {
   adapter = require('./redis');
+} else if (config.storage.type === 'azureblob') {
+  adapter = require('./azureBlob');
 } else {
   throw new Error(`Unrecognised storage type ${config.storage.type}in config`);
 }

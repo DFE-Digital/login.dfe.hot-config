83dab508f477a1178ade30e4e7eb432f891bdba8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-21 08:02:17 +0000

    Set keep alive settings from config

--- src/index.js
+++ src/index.js
@@ -20,16 +20,16 @@ const { hotConfigSchema, validateConfigAndQuitOnError } = require('login.dfe.con
 validateConfigAndQuitOnError(hotConfigSchema, config, logger);
 
 http.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 160,
-  maxFreeSockets: 10,
-  timeout: 60000,
-  keepAliveTimeout: 300000,
+  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
 });
 https.GlobalAgent = new KeepAliveAgent({
-  maxSockets: 160,
-  maxFreeSockets: 10,
-  timeout: 60000,
-  keepAliveTimeout: 300000,
+  maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+  maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+  timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+  keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
 });
 
 const app = express();

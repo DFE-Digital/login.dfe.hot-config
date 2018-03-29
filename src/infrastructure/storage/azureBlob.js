dff25e11771ceb4b34f5021876ff91dd0485aaff
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-03-29 15:22:01 +0100

    Add agentkeepalive to request-promise init

--- src/infrastructure/storage/azureBlob.js
+++ src/infrastructure/storage/azureBlob.js
@@ -2,9 +2,14 @@
 
 const config = require('./../config');
 const logger = require('./../logger');
+const KeepAliveAgent = require('agentkeepalive').HttpsAgent;
 const rp = require('request-promise').defaults({
-  forever: true,
-  keepAlive: true,
+  agent: new KeepAliveAgent({
+    maxSockets: config.hostingEnvironment.agentKeepAlive.maxSockets,
+    maxFreeSockets: config.hostingEnvironment.agentKeepAlive.maxFreeSockets,
+    timeout: config.hostingEnvironment.agentKeepAlive.timeout,
+    keepAliveTimeout: config.hostingEnvironment.agentKeepAlive.keepAliveTimeout,
+  }),
 });
 
 const getOIDCClients = async (correlationId) => {
